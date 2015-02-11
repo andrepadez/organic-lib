fd4cf79593187ee2bf2fcfed0bdf5f15fcfa2f21
André Alçada Padez <andre.padez@gmail.com>
2015-02-11 13:55:48 +0100

    lib started

---
+++ ajax.js
@@ -0,0 +1,22 @@
+var Q = require('q');
+
+var Ajax = module.exports = {
+
+    get: function(url){
+        var deferred = Q.defer();
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function(ev){
+            if(this.status < 400){
+                deferred.resolve(this.responseText);    
+            } else {
+                deferred.reject({
+                    status: this.status,
+                    message: this.responseText
+                });
+            }
+        };
+        xhr.send();
+        return deferred.promise;
+    }
+};
---
+++ templator.js
@@ -0,0 +1,41 @@
+var Q = require('q');
+var ajax = require('../lib/ajax');
+
+var cache = { regExp: {}, templates: {} };
+
+var Templator = module.exports = {
+    render: function(url, data){
+        data = data || {};
+        var deferred = Q.defer();
+        ajax.get(url).then(function(res){
+            cache.templates[url] = res;
+            var supplanted = Templator.supplant(res, data);
+            deferred.resolve(supplanted);
+        }, function(err){
+            deferred.reject(err);
+        });
+        return deferred.promise;
+    },
+    renderQueue: function(url, queue){
+        var promises = [];
+        queue.forEach(function(data){
+            promises.push(Templator.render(url, data));
+        });
+        return Q.all(promises).then(function(res){
+            return res.join('');
+        });
+    },
+    renderHTML: function(html, data){
+        var deferred = Q.defer();
+
+        return deferred.promise();
+    },
+    supplant: function(html, data){
+        Object.keys(data).forEach(function(key){
+            var regExp = cache[key] || new RegExp('\{\{' + key + '\}\}', 'g');
+            cache[key] = regExp;
+            html = html.replace(regExp, data[key]);
+        });
+        return html;
+    }
+};
