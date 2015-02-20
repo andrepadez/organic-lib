643857a04df1baed6951cb82a5d393d6d35c1a3f
André Alçada Padez <andre.padez@gmail.com>
2015-02-20 21:57:45 +0100

    added image type

--- image/index.js
+++ image/index.js
@@ -1,21 +1,26 @@
-var MyImage = module.exports = function(file){
+var Image = module.exports = function(file){
     this.file = file;
     Object.keys(this.file).forEach( function(key){
         this[key] = file[key];
     }.bind(this) );
 };
 
-MyImage.prototype.load = function(){
+Image.prototype.load = function(){
     var deferred = Q.defer();
+    
     var reader = new FileReader();
+
     reader.onload = function(ev){
         this.dataUrl = ev.target.result;
         console.log('loaded', this.name);
         deferred.resolve(this);
     }.bind(this);
+
     reader.onerror = function(ev){
         deferred.reject( {message: 'can\'t load image: ' + this.name } );
     }.bind(this);
+
     reader.readAsDataURL(this.file);
+
     return deferred.promise;
 };
