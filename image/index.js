f8ad5087dca280d53b661542a9c24781e7d1cfb7
André Alçada Padez <andre.padez@gmail.com>
2015-03-01 22:09:42 +0100

    added resizeToCanvas

--- image/index.js
+++ image/index.js
@@ -1,6 +1,9 @@
 var Q = require('q');
+var assert = require('chai').assert;
+var pica = require('pica');
 
-var Image = function(file){
+var OETImage = function(file){
+    this.complete = false;
     this.file = file;
     this.image = new window.Image();
     Object.keys(this.file).forEach( function(key){
@@ -8,10 +11,10 @@ var Image = function(file){
     }.bind(this) );
 };
 
-Image.prototype.constructor = Image;
-module.exports = Image;
+OETImage.prototype.constructor = Image;
+module.exports = OETImage;
 
-Image.prototype.load = function(){
+OETImage.prototype.load = function(){
     var deferred = Q.defer();
 
     var reader = new FileReader();
@@ -19,6 +22,7 @@ Image.prototype.load = function(){
     reader.onload = function(ev){
         this.dataUrl = ev.target.result;
         this.image.onload = function(){
+            this.complete = true;
             deferred.resolve(this);
         }.bind(this);
         this.image.src = this.dataUrl;
@@ -32,3 +36,50 @@ Image.prototype.load = function(){
 
     return deferred.promise;
 };
+
+OETImage.prototype.getRatio = function(){
+    return this.image.width / this.image.height;
+};
+
+OETImage.prototype.resizeToCanvas = function(options){
+    var deferred = Q.defer();
+    var targetWidth = options.targetWidth;
+    var targetHeight = options.targetHeight;
+    if( !(targetWidth || targetHeight) ){
+        var err = Error('you have to specify a targetWidth and/or targetHeight');
+        console.error(err);
+        return deferred.reject( err );
+    }
+
+    var render = function(){
+        var originalCanvas = document.createElement('canvas');
+        originalCanvas.width = this.image.width;
+        originalCanvas.height = this.image.height;
+        var ctx = originalCanvas.getContext('2d');
+        ctx.drawImage(this.image, 0, 0, this.image.width, this.image.height);
+        
+        var ratio = this.getRatio();
+        var resizedCanvas = document.createElement('canvas');
+        resizedCanvas.width = targetWidth || targetHeight * ratio;
+        resizedCanvas.height = targetHeight || targetWidth * ratio;
+
+        var picaOptions = {
+            quality: options.quality || 0,
+            unsharpAmount: options.unsharpAmount || 50,
+            unsharpThreshold: options.unsharpThreshold || 10
+        };
+
+        pica.resizeCanvas(originalCanvas, resizedCanvas, options, function(err){
+            deferred.resolve(resizedCanvas);
+        });
+    }
+
+    if(this.complete){
+        render.call(this);
+    } else {
+        this.load().then( render.bind(this) );
+    }
+
+    return deferred.promise;
+};
+
