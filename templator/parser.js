6182aaa51184d194d9ece7126b1ee6cea98dd432
André Alçada Padez <andre.padez@gmail.com>
2015-03-11 14:48:53 +0100

    parser.parseValues corrected

--- templator/parser.js
+++ templator/parser.js
@@ -8,23 +8,38 @@ var Parser = module.exports = {
 
 var evaluate = function(template, data){
     template = parseValues(template, data);
+    template = parseExpressions(template, data);
     console.log(template);
     return template;
 };
 
 var parseValues = function(template, data){
-    var execResult;
+    var execResult, toReplace, expression, expressionArray;
     var values = {};
+    var finalTemplate = template;
     while(execResult = regExps.value.exec(template)){
-        var expression = execResult[1].trim();
-        var toReplace = execResult[0];
-        template = template.replace( toReplace, data[expression] || toReplace );
+        expressionArray = execResult[1].trim().split('.');
+        expression = data[ expressionArray.shift() ];
+        
+        expressionArray.forEach(function(key, index){
+            expression = expression[ key ];
+            if(!expression){
+                throw Error('can\'t find ' + execResult[1].trim() + ' while rendering template');
+            }
+        });
+
+        toReplace = execResult[0];
+        finalTemplate = finalTemplate.replace( toReplace, expression || toReplace );
     }
-    return template;
+    return finalTemplate;
 };
 
+var parseExpressions = function(template, data){
+    return template;
+}
+
 var regExps = {
-    value: /\{\{(.+)\}\}/g,
-    expression: /\{\%(.+)\%\}/g
+    value: /\{\{([^\{\}]+)\}\}/g,
+    expression: /\{\%([^\{\%\}])\%\}/g
 };
 
