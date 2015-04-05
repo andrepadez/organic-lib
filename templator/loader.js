c96fc23bcd0c39c4fa6262c87b27dc0a3b2162c9
André Padez <andre.padez@gmail.com>
2015-04-05 20:09:08 +0200

    added post to ajax

--- templator/loader.js
+++ templator/loader.js
@@ -1,33 +1,47 @@
+// promised library for Ajax requests
 var Q = require('q');
-var ajax = require('../ajax');
 
-var Loader = module.exports = {
-    init: function(views){
-        this.views = views;
+var Ajax = module.exports = {
+
+    get: function(url){
+        var deferred = Q.defer();
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = xhrOnloadHandler.bind(xhr, deferred);
+        xhr.send();
+
+        return deferred.promise;
     },
-    load: function(url){
-        if( Loader.views ){
-            return loadFromJSON(url);
-        } else {
-            return ajax.get(url);
-        }
+    getJSON: function(url){
+        return Ajax.get(url).then(JSON.parse);
+    }, 
 
-        
+    post: function(url, payload){
+        var deferred = Q.defer();
+
+        var formData = new FormData();
+        Object.keys(payload).forEach(function(key){
+            formData.append( key, payload[key] );
+        });
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.onload = xhrOnloadHandler.bind(xhr, deferred);
+        xhr.send( JSON.stringify(payload) );
+
+        return deferred.promise;
     }
 };
 
-var loadFromJSON = function(url){
-    var deferred = Q.defer();
-    path = url.split('/');
-    var template = Loader.views;
-    Object.keys(path).forEach(function(key){
-        if(key !== "0" || path[key] !== 'views'){
-            template = template[ path[key] ];
-            if(!template){
-                deferred.reject( Error('view not found in views.json ' + url) );
-            }
-        }
-    });
-    deferred.resolve( template );
-    return deferred.promise;
-}; 
+var xhrOnloadHandler = function(deferred, ev){
+    if(this.status < 400){
+        deferred.resolve(this.responseText);    
+    } else {
+        deferred.reject({
+            status: this.status,
+            message: this.responseText
+        });
+    }
+};
